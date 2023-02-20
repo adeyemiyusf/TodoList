6b2612312990474c6efb639f45162cacf6904c65
adeyemiyusf <adeyemiyusuff05@gmail.com>
2023-02-20 12:39:01 +0100

    updated todoList

--- src/pages/Todopage.js
+++ src/pages/Todopage.js
@@ -1,21 +1,77 @@
 import {useState} from "react";
 const Todopage = () => {
-    return ( 
-        const [todoList, setTodoList] = useState([]);
-        const [newTask, setNewTask] = useState("");
+    const [todoList, setTodoList] = useState([]);
+    const [newTask, setNewTask] = useState ("");
 
-        const handleChange = (event) =>{
-            setNewTask(event.target.value);
-       }
-    )
+    const handleChange = (event) =>{
+        setNewTask(event.target.value);
+   }
+   
 
-    const addTask = () => {
-        const task = {
-            id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+   const addTask = () => {
+    // const newTodoList = [...todoList, newTask];
+
+    const task = {
+        id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
         taskName: newTask,
-        }
-        setTodoList ([...todoList, task])
     }
+
+    setTodoList ([...todoList, task])
+   }
+    
+
+   const deleteTask = (id) => {
+    //   const newTodoList = todoList.filter((task) => task !== taskName)
+    //   {
+    //     if (task === taskName){
+    //         return false
+    //     }else {
+    //         return true;
+    //     }
+    //   }
+      
+
+      setTodoList(todoList.filter((task) => task.id !== id))
+   }
+
+   const completeTask = (id) => {
+    setTodoList(
+        todoList.map((task) => {
+             if(task.id ===id) {
+                return {...task, completed: true};
+             }else{
+                return task
+             }
+        })
+    )
+   }
+
+
+    return (  
+        <div>
+        <div className="addTask">
+
+            <input onChange={handleChange}/>
+            <button onClick={addTask}>Add Task</button>
+            </div>
+            <div className="List">
+                {todoList.map((task) => {
+                    return (
+                        <Task
+                        taskName={task.taskName}
+                        id = {task.id}
+                        completed={task.completed}
+                        deleteTask={deleteTask}
+                        completeTask={completeTask}
+                        />
+                    )
+                })}
+
+            </div>
+
+        
+        </div>
+    );
 }
  
 export default Todopage;
